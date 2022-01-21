2ac98a2ba5e0ff8e5e581166b87e380026bbee2a
adamhinton <adam.d.hinton@gmail.com>
2022-01-20 23:27:35 -0500

    built tasks table migration

--- data/migrations/20220121010111_initial-migration.js
+++ data/migrations/20220121010111_initial-migration.js
@@ -12,11 +12,27 @@ exports.up = async function (knex) {
     table.string("resource_description", 200);
   });
 
-  // await knex.schema.createTable("", (table) => {});
+  await knex.schema.createTable("tasks", (table) => {
+    table.increments("task_id");
+    table.string("task_description").notNullable();
+    table.string("task_notes");
+    table.boolean("task_completed").defaultTo(0);
+    //foreign key now
+    table
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("project_id")
+      .inTable("projects")
+      .onDelete("RESTRICT")
+      .onUpdate("RESTRICT");
+  });
 
   // await knex.schema.createTable("", (table) => {});
 };
 
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("projects");
 };
