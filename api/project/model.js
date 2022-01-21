b19b361b69131f179fa595701d9644019e5db572
adamhinton <adam.d.hinton@gmail.com>
2022-01-21 10:38:46 -0500

    projects get is passing tests

--- api/project/model.js
+++ api/project/model.js
@@ -1,12 +1,35 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig");
 
+function getProjects() {
+  return db("projects");
+}
+
 async function getProjectById(project_id) {
-  const projectRows = await db("projects").where("project_id", project_id);
+  const projectRows = await db("projects")
+    .select()
+    .where("project_id", project_id);
 
   return projectRows;
 }
 
-module.exports = {
-  getProjectById,
+const fixBoolean = (items) => {
+  items.forEach((item) => {
+    item.project_completed
+      ? (item.project_completed = true)
+      : (item.project_completed = false);
+  });
 };
+
+module.exports = {
+  getProjects,
+  getProjectById,
+  fixBoolean,
+};
+
+// const fixBoolean = items =>{
+//   items.forEach(item) =>{
+//     item.project_completed ? proj.project_completed = true
+//     : proj.project_completed = false;
+//   }
+// }
