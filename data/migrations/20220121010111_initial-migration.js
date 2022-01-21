11247d045cc95bd8f773b936ec9e53067c439457
adamhinton <adam.d.hinton@gmail.com>
2022-01-21 09:09:52 -0500

    seeds up and running

--- data/migrations/20220121010111_initial-migration.js
+++ data/migrations/20220121010111_initial-migration.js
@@ -8,7 +8,7 @@ exports.up = async function (knex) {
 
   await knex.schema.createTable("resources", (table) => {
     table.increments("resource_id");
-    table.string("resource name", 35).notNullable().unique();
+    table.string("resource_name", 35).notNullable().unique();
     table.string("resource_description", 200);
   });
 
--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -16,8 +16,38 @@ const projects = [
   },
 ];
 
-const resources = [{ resource }];
+const resources = [
+  { resource_name: "judo classes", resource_description: "take judo classes" },
+  {
+    resource_name: "fake dummies",
+    resource_description: "need to acquire this",
+  },
+  { resource_name: "ski masks", resource_description: "disguise" },
+];
+
+const tasks = [
+  {
+    task_description: "find nearest little kid",
+    task_notes: "beat up said little kid",
+    task_completed: false,
+    project_id: 1,
+  },
+  {
+    task_description: "sneak out of house",
+    task_notes: "do it quiet",
+    task_completed: false,
+    project_id: 2,
+  },
+  {
+    task_description: "Get evil money bag",
+    task_notes: "Use to rob bank",
+    task_completed: false,
+    project_id: 3,
+  },
+];
 
 exports.seed = async function (knex) {
   await knex("projects").insert(projects);
+  await knex("resources").insert(resources);
+  await knex("tasks").insert(tasks);
 };
