da54bee9810622f3092874868f12e44a6b909c78
adamhinton <adam.d.hinton@gmail.com>
2022-01-21 10:38:36 -0500

    projects get is passing tests

--- api/project/router.js
+++ api/project/router.js
@@ -1,9 +1,24 @@
+// import { types } from "pg";
+// types.setTypeParser(16, (value) => {
+//   // 16 is the type enum vaue of boolean
+//   return Boolean(parseInt(value));
+// });
+
 // build your `/api/projects` router here
 const router = require("express").Router();
 const Project = require("./model");
 
 router.get("/", (req, res, next) => {
-  res.json("get up and running");
+  Project.getProjects()
+    .then((projects) => {
+      Project.fixBoolean(projects);
+      res.json(projects);
+    })
+    .catch(next);
 });
+
+// router.post("/", (req, res, next) =>{
+
+// }
 
 module.exports = router;
